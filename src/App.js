45723c27a7f1ede30cd9e80a5de30033f324daf4
Amen Christ <amenphronesis@gmail.com>
2023-10-27 08:12:03 +0100

    Title change

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           PREPARING FOR THE MILLENIAL REIGN
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
