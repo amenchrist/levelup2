35bc84225f1c1fa13da83fc942aa8ea45669a7be
Amen Christ <amenphronesis@gmail.com>
2024-08-08 22:53:41 +0100

    fleshing out v3 home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,53 +10,57 @@ import DailyExercises from '../components/DailyExercises';
 import SomedayOverview from '../components/SomedayOverview';
 import ReferencesOverview from '../components/ReferencesOverview';
 import EventsOverview from '../components/EventsOverview';
+import List from '../components/List';
+import Scroll from '../components/Scroll';
+import { useMyStore } from '../store';
+import ListItem from '../components/ListItem';
 
 export default function Home() {
+    let content = useMyStore(store => store['tasks']);
+    let listItems;
+    
+    if(content){
+        listItems = content.map((entry,i) => {
+            return <ListItem item={content[i]} key={content[i].id}/>
+        })
+    }
+
+    function ListContainer({children}) {
+        return (
+            <div className='h-100 pa2'>
+                {/* <div className='h-10'>
+                    <h5 className='fw3 white'>EXP: {10}</h5>
+                </div> */}
+                <div className='h-90 pa1'>
+                    <div className='h-100 w-100 center pa1'>
+                        {/* <h1 className='tc b gold ma0 pb2'>{title}</h1> */}
+                        <h2 className='tc b gold f3'>{'Next Action'}</h2>
+                        <div className=' h-80 '>
+                        {children}
+                        </div>
+                        <div className='h-10 flex w-100 content-end pa2'>
+                            {/* <NewItemButton touchFunsction={handleEvent} /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>   
+        )
+    }
    
     return (
         <div className='h-100 pa1' >
-            <div className='pa1 ph1 h-40 ba'>
-                <StatsOverview  />
-            </div>
-            <div className="flex justify-center h-20 ">
-                <div className='w-50 pa1'>
-                    <MissionsOverview  />
-                </div>
-                <div className='w-50 pa1'>
-                    <TaskOverview />
-                </div>
-            </div>
-            <div className='flex w-100 h-10 pa1'>
-                <NewItemTile />
-            </div>
-            <div className="flex justify-center h-30">
-                <div className='w-50 h-100 pa1'>
-                    <div className=' w-100 h-50 pb1'>
-                        <TodaysMission />
-                    </div>
-                    <div className=' w-100 h-50 pt1'>
-                        <DailyExercises />
-                    </div>
-                </div>
-                <div className=' w-50 h-100'>
-                    <div className=' w-100 h-50'>
-                        <div className=' h-50 pa1'>
-                            <SomedayOverview />
-                        </div>
-                        <div className='h-50 pa1'>
-                            <ReferencesOverview />
-                        </div>
-                    </div>
-                    <div className='w-100 h-50 flex'>
-                        <div className='w-50 h-100 pa1' >
-                            <EventsOverview />
-                        </div>
-                        <div className='w-50 h-100 pa1'>
-                            <InboxOverview />
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <h1 className='white b '>Christ Amen</h1>
+            <p className='white'>Rank: F</p>
+            <p className='white'>Class: God</p>
+            <p className='white'>Exp: 2356</p>
+            <hr/>
+            <ListContainer>
+                    <p className='white'>Total: {0}</p>
+                    <Scroll>
+                        {listItems}
+                    </Scroll>
+                </ListContainer>
+
         </div>
     );
 }
