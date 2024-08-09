806cb8735fb8f10cb0bb3fb1de8ae9055f25057d
Amen Christ <amenphronesis@gmail.com>
2024-08-09 10:45:30 +0100

    fleshing out v3 home

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -28,14 +28,14 @@ export default function Home() {
 
     function ListContainer({children}) {
         return (
-            <div className='h-100 pa2'>
+            <div className='h-90 pa2'>
                 {/* <div className='h-10'>
                     <h5 className='fw3 white'>EXP: {10}</h5>
                 </div> */}
                 <div className='h-90 pa1'>
                     <div className='h-100 w-100 center pa1'>
                         {/* <h1 className='tc b gold ma0 pb2'>{title}</h1> */}
-                        <h2 className='tc b gold f3'>{'Next Action'}</h2>
+                        <h2 className='b gold f3'>{'Next Task'}</h2>
                         <div className=' h-80 '>
                         {children}
                         </div>
@@ -50,25 +50,32 @@ export default function Home() {
    
     return (
 			<div className='h-100 pa1' >
-					<Container component="main" maxWidth="xs" sx={{}}>
-						<Box sx={{ marginTop: 4, display: 'flex', flexDirection: 'column',height:'80%', }} color={'white'}>
-							<Typography component="h1" variant="h5" >Amen Christ</Typography>
-							<Grid container justifyContent="space-between">
-								<Grid item>
-									<Typography variant='p' >Rank: F</Typography>
-								</Grid>
-								<Grid item>
+					<Container component="main" maxWidth="xs" sx={{paddingBottom: '10px'}}>
+						<Box sx={{ marginTop: 3, display: 'flex', flexDirection: 'column',height:'80%', }} color={'white'}>
+							<Typography component="h1" variant="h5" sx={{ fontWeight: 'bold' }}>Amen Christ</Typography>
+							{/* <Grid container alContent="space-between" sx={{padding: '5px 0 10px 0'}}>
+							</Grid> */}
+								<Grid item sx={{padding: '5px 0 5px 0'}}>
 									<Typography variant='p' >Class: God</Typography>
 								</Grid>
-							</Grid>
-							<Grid item>
+								<Grid item sx={{padding: '5px 0 5px 0'}}>
+									<Typography variant='p'  >Rank: F</Typography>
+								</Grid>
+							<Grid item sx={{padding: '5px 0 5px 0'}}>
 							<Typography variant='p' >Exp: 2356</Typography>
 						</Grid>
 				</Box>
       </Container>
             <hr/>
             <ListContainer>
-                    <p className='white'>Total: {0}</p>
+						<Grid container justifyContent="space-between" alignItems={'center'} color={'white'} sx={{padding: '20px 0 10px 0'}}>
+								<Grid item>
+									<Typography variant='p' >Total: 0</Typography>
+								</Grid>
+								<Grid item sx={{border: '2px solid white', padding: '5px 7px'}}>
+									<Typography variant='p' >+</Typography>
+								</Grid>
+							</Grid>
                     <Scroll>
                         {listItems}
                     </Scroll>
