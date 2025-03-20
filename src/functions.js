b4d90e49c18f032b8f03b69e1d7e9e381a9b0f76
Amen Christ <amenphronesis@gmail.com>
2025-03-20 01:06:15 +0000

    Fixed scheduler to account for events

--- src/functions.js
+++ src/functions.js
@@ -257,6 +257,8 @@ export function reSchedule(task, updateFunc) {
         
     })
 
+    const firstActivityTime = new Date(tasks[0].scheduledDate).getTime()
+
 
     if(tasks.length === 0 ) {
         date = new Date(next5).toString();
@@ -264,15 +266,15 @@ export function reSchedule(task, updateFunc) {
         task.setScheduledDate(date);
         task.setScheduledTime(time);
         updateFunc(task);
-    } else if (new Date(tasks[0].scheduledDate).getTime() - next5+buffer >= (task.timeRequired*60*1000 + buffer) ) {
-        date = new Date(next5+buffer).toString();
+    } else if (recommendedPredecessor) {
+        const scheduledDate = new Date(recommendedPredecessor.scheduledEndDate).getTime()+buffer
+        date = new Date(scheduledDate).toString();
         time = dayjs(date).format('HH:mm')
         task.setScheduledDate(date);
         task.setScheduledTime(time);
         updateFunc(task);
-    } else if (recommendedPredecessor) {
-        const scheduledDate = new Date(recommendedPredecessor.scheduledEndDate).getTime()+buffer
-        date = new Date(scheduledDate).toString();
+    } else if (firstActivityTime - next5+buffer >= (task.timeRequired*60*1000 + buffer)) {
+        date = new Date(next5+buffer).toString();
         time = dayjs(date).format('HH:mm')
         task.setScheduledDate(date);
         task.setScheduledTime(time);
