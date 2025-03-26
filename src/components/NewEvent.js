eed4bff183819b1cf644b83d6d3bc7d02142c289
Amenchrist <christamen@christ.university>
2025-03-26 18:47:37 +0000

    Working on daily events

--- src/components/NewEvent.js
+++ src/components/NewEvent.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { ADD, DAILY, DETAILS, EVENTS, MONTHLY, NONE, WEEKLY, YEARLY } from '../constants';
+import { Box, Button, Checkbox, Container, FormControlLabel, Grid, MenuItem, TextField, Typography } from '@mui/material';
 import {  Event } from '../classes/Event';
 import { useMyStore } from '../store';
 import { useNavigate } from 'react-router-dom';
@@ -21,43 +22,104 @@ export default function NewEvent({ item, processorSubmit }) {
     const [ endDate, setEndDate ] = useState(dayjs().format('YYYY-MM-DD'));
     const [ endTime, setEndTime ] = useState('');
     const [ location, setLocation ] = useState("");
-    const [ frequency, setFrequency ] = useState("");
+    const [ frequency, setFrequency ] = useState("ONE-TIME");
     const [ note, setNote ] = useState('');
     const [ duration, setDuration ] = useState('');
 
+    const [ recurring, setRecurring ] = useState(false);
+    const [ reEndDate, setReEndDate ] = useState(dayjs().format('YYYY-MM-DD'));
 
+    const frequencyOptions = [ {value: 'DAILY', label: 'Daily'}, {value: 'WEEKLY', label: 'Weekly'}, {value: 'MONTHLY', label: 'Monthly'} ];
 
     function submitNewItem(event) {
         event.preventDefault();
 
-        const startDate = dayjs(`${date} ${time}`).toDate().toString();
-        const end = new Date(startDate).getTime()+(duration*60000)
+        const startDate = dayjs(`${date} ${time}`).toDate();
+        const createEvent = (start) => {
+            const end = new Date(start.toString()).getTime()+(duration*60000);
+            let e = new Event(
+                {
+                    name, scheduledDate: start, 
+                    time, location, frequency: recurring? frequency : 'ONE-TIME', 
+                    scheduledEndDate: dayjs(end).toDate().toString(),
+                });
+            addItem(e);
+            return e
+        }
 
-        let e = new Event(
-            {
-                name, scheduledDate: startDate, 
-                time, location, frequency, 
-                scheduledEndDate: dayjs(end).toDate().toString(),
-            });
-        addItem(e);
+        // let e = new Event(
+        //     {
+        //         name, scheduledDate: startDate, 
+        //         time, location, frequency, 
+        //         scheduledEndDate: dayjs(end).toDate().toString(),
+        //     });
+        // addItem(e);
         
-        if (frequency === DAILY ){
-            const recurrenceLimit = 5;
-            for (let i=1; i<=recurrenceLimit; i++){
-                let nextDate = dayjs(`${date} ${time}`).add(i, 'day').toDate().toString();
-                let nextEnd = dayjs(`${endDate} ${endTime}`).add(i, 'day').toDate().toString();
+        // if (frequency === DAILY ){
+        //     const recurrenceLimit = 5;
+        //     for (let i=1; i<=recurrenceLimit; i++){
+        //         let nextDate = dayjs(`${date} ${time}`).add(i, 'day').toDate().toString();
+        //         let nextEnd = dayjs(`${endDate} ${endTime}`).add(i, 'day').toDate().toString();
 
-                const e1 =  {name, scheduledDate: nextDate, scheduledEndDate: nextEnd, time, location, frequency};
-                addItem({...new Event(e1)});
+        //         const e1 =  {name, scheduledDate: nextDate, scheduledEndDate: nextEnd, time, location, frequency};
+        //         addItem({...new Event(e1)});
+        //     }
+        // }
+
+        if(recurring){
+            const start = dayjs(`${date} ${time}`).toDate();
+            const current = dayjs(`${date} ${time}`).toDate();
+            const end = dayjs(`${reEndDate} ${time}`).toDate();
+      
+            const dates = [];
+            while (current <= end){
+              switch(frequency){
+                // case 'DAILY':
+                //   if(current.getDay() === start.getDay()){
+                //     dates.push(dayjs(new Date(current)).format('YYYY-MM-DD'))
+                //   }
+                // //   current.setDate(current.getDate()+ 1)
+                // console.log(current.getDate())
+                //   break;
+                case 'WEEKLY':
+                  if(current.getDay() === start.getDay()){
+                    dates.push(dayjs(new Date(current)).format('YYYY-MM-DD'))
+                  }
+                  current.setDate(current.getDate()+ 1)
+                  break;
+                case 'MONTHLY':
+                  //SAME Day EVERY MONTH
+                  if(current.getDate() === start.getDate()){
+                    dates.push(dayjs(new Date(current)).format('YYYY-MM-DD'))
+                  }
+                  current.setMonth(current.getMonth() + 1);
+                  current.setDate(1);
+                  break;
+                default:
+                  //Every day till end date
+                  dates.push(dayjs(new Date(current)).format('YYYY-MM-DD'))
+                  current.setDate(current.getDate()+ 1)
+              }
             }
-        } 
+      
+            dates.forEach(d => {
+              createEvent(d)
+            // console.log(d)
+            } )
+
+            navigate(`/Events`);
+      
+          } else {
+            const e = createEvent(startDate)
+            navigate(`/Events/${e.id}`);
+          }
 
         if(item){
-            processorSubmit(e.id)
+            // processorSubmit(e.id)
         }
         updateItem(player.updateExp(5))
         
-        navigate(`/Events/${e.id}`);
+        
         rescheduleAll(updateItem);
     }
 
@@ -81,15 +143,37 @@ export default function NewEvent({ item, processorSubmit }) {
 
                 <label className='fw4 white' htmlFor="location" >Location:</label>
                 <input className='pa2 mb1' type='text' placeholder='Location' value={location} onChange={(e) => setLocation(e.target.value)} />
-                <label className='fw4 white' htmlFor="frequency" >Frequency:</label>
-                <select className='pa2 mb1' id="priority" value={frequency} onChange={(e)=> setFrequency(e.target.value)}>
-                    {/* <option value="" disabled >Frequency</option> */}
+                <Grid item xs={12} >
+                  <FormControlLabel control={<Checkbox onChange={() => setRecurring(!recurring)} checked={recurring} sx={{color: 'white'}} /> }  label="Recurring" sx={{color: 'white'}} />
+                </Grid>
+                { !recurring? <></> :
+                <>
+                  <Grid item xs={12} sm={6} >
+                    <label className='fw4 white' htmlFor="frequency" >Frequency:</label>
+                    <TextField required={recurring} fullWidth select value={frequency} onChange={(e) => setFrequency(e.target.value)} sx={{color: 'white', bgcolor: 'white', mb: 2}} >
+                      {frequencyOptions.map((e,i) => (
+                        <MenuItem key={i} value={e.value} sx={{textColor: 'white', mb: 2}}>{e.value}</MenuItem>
+                      ))}
+                    </TextField>
+                  </Grid>
+                  <Grid item xs={12} sm={6} >
+                    <label className='fw4 white' htmlFor="frequency" >End Date:</label>
+                    <TextField required fullWidth type="date" value={reEndDate} onChange={(e) => setReEndDate(e.target.value)} sx={{bgcolor: 'white', mb: 2}} />
+                  </Grid>
+                </>              
+                }
+                {/* <Grid item xs={12} >
+                <TextField required fullWidth label="Watch Link" id="watch-link" value={watchLink} onChange={(e) => setWatchLink(e.target.value)}/>
+                </Grid> */}
+                {/* <label className='fw4 white' htmlFor="frequency" >Frequency:</label> */}
+                {/* <select className='pa2 mb1' id="priority" value={frequency} onChange={(e)=> setFrequency(e.target.value)}>
+                    <option value="" disabled >Frequency</option>
                     <option value={NONE} defaultValue>ONE-TIME</option>
                     <option value={DAILY}>DAILY</option>
                     <option value={WEEKLY}>WEEKLY</option>
                     <option value={MONTHLY}>MONTHLY</option>
                     <option value={YEARLY}>YEARLY</option>
-                </select>
+                </select> */}
                 <textarea className='pa2 mb1' placeholder='Note' value={note} onChange={(e) => setNote(e.target.value)} />
                 <input className='pa2 mb1'type='submit' value='submit' />
             </form>
